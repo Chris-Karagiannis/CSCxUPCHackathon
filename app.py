73d12cecb0c127b26a54f93b13cb6ab36416f15c
Kimchhay Leng <150762525+ChhayZ7@users.noreply.github.com>
2025-09-26 16:44:48 +0930

    Adding cart page

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.jinja")
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/Cart")
+def Cart():
+    return render_template("cart.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
