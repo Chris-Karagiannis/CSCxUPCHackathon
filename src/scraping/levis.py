3733058f86a13db3f59713719db22ea6ad62c5cf
Chris-Karagiannis <chris.karagiannis30@outlook.com>
2025-09-27 11:13:23 +0930

    Add Factorie scraping

--- src/scraping/levis.py
+++ src/scraping/levis.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 from scrape import Scrape
 
